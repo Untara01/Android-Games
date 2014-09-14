9cc003d8adc0ee12124ce5a5aca5c381b7ad8a96
Jaewon Yang <jaewonmichael@gmail.com>
2014-09-14 16:18:04 -0700

    OCD
    ocd

--- SpaceShooter/android/src/com/jae/spaceshooter/android/AndroidLauncher.java
+++ SpaceShooter/android/src/com/jae/spaceshooter/android/AndroidLauncher.java
@@ -6,7 +6,8 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.jae.spaceshooter.SpaceShooter;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication 
+{
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
