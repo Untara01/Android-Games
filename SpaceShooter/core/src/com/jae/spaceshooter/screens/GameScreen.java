a391dde92b0737e113a3b6f8339f14fda4c87386
Jaewon Yang <jaewonmichael@gmail.com>
2014-09-14 17:51:22 -0700

    OCD

--- SpaceShooter/core/src/com/jae/spaceshooter/screens/GameScreen.java
+++ SpaceShooter/core/src/com/jae/spaceshooter/screens/GameScreen.java
@@ -2,12 +2,9 @@ package com.jae.spaceshooter.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.jae.spaceshooter.SpaceShooter;
